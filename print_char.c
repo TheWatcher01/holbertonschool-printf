b001d59d332f66c92627207d3676bd927bf0f81d
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-24 15:24:04 -0800

    print_char.c_v2

--- print_char.c
+++ print_char.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * print_char - prints a character
- * @va: argument that gives a char
- * Return: 1
+ * print_char - Outputs a single character to the console.
+ *
+ * @va: A variable argument list containing the character to print.
+ *
+ * Return: 1 (number of characters printed).
  */
+
 int print_char(va_list va)
 {
 	_putchar((char) va_arg(va, int));
